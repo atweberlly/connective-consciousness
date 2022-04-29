bb4933df225b3b320a3cc8986358d15e7f5ebbde
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2022-04-29 22:12:41 +0800

    Create Our Products section

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,3 +1,43 @@
 <template>
-  <main></main>
+  <main>
+    <section class="py-8">
+      <div class="container">
+        <div class="text-center uppercase tracking-wider">Our Products</div>
+        <h2 class="text-center font-display text-2xl font-bold text-sky-blue">The products we offer to you.</h2>
+        <div class="mt-8 sm:mx-auto sm:max-w-sm lg:max-w-4xl" role="grid">
+          <div class="grid gap-8 lg:grid-cols-3" role="row">
+            <div role="gridcell">
+              <div class="bg-cyprus text-white" role="article">
+                <div class="aspect-video bg-slate-400"></div>
+                <div class="px-6 py-4">
+                  <h3 class="font-display text-lg font-bold">Women Rising Book</h3>
+                  <p class="mt-6 text-slate-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, repudiandae sequi nulla obcaecati non unde sit laborum, animi ad, ipsum incidunt tempora eius esse.</p>
+                </div>
+              </div>
+            </div>
+
+            <div role="gridcell">
+              <div class="bg-cyprus text-white" role="article">
+                <div class="aspect-video bg-slate-400"></div>
+                <div class="px-6 py-4">
+                  <h3 class="font-display text-lg font-bold">Women Rising Book</h3>
+                  <p class="mt-6 text-slate-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, repudiandae sequi nulla obcaecati non unde sit laborum, animi ad, ipsum incidunt tempora eius esse.</p>
+                </div>
+              </div>
+            </div>
+
+            <div role="gridcell">
+              <div class="bg-cyprus text-white" role="article">
+                <div class="aspect-video bg-slate-400"></div>
+                <div class="px-6 py-4">
+                  <h3 class="font-display text-lg font-bold">Women Rising Book</h3>
+                  <p class="mt-6 text-slate-400">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores, repudiandae sequi nulla obcaecati non unde sit laborum, animi ad, ipsum incidunt tempora eius esse.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
 </template>
