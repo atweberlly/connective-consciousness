4e601836eaafb8c586cddec687ded73deabd475a
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2022-04-29 16:49:30 +0800

    Update mobile and desktop header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
   <header class="py-6">
-    <div class="container">
+    <div class="container flex items-center justify-between">
       <div>
         <span class="sr-only">Connective Consciousness</span>
         <a class="inline-block" href="/">
@@ -8,14 +8,22 @@
         </a>
       </div>
 
-      <nav class="hidden">
-        <a href="/">Home</a>
-        <a href="/">About</a>
-        <a href="/">Blog</a>
-        <a href="/">Contact</a>
-        <a href="/">Shop</a>
-        <a href="/">Book Online</a>
+      <nav class="hidden md:block md:space-x-4" aria-label="Header Menu">
+        <a class="md:text-sm md:uppercase" href="/">Home</a>
+        <a class="md:text-sm md:uppercase" href="#">About</a>
+        <a class="md:text-sm md:uppercase" href="#">Blog</a>
+        <a class="md:text-sm md:uppercase" href="#">Contact</a>
+        <a class="md:text-sm md:uppercase" href="#">Shop</a>
+        <a class="md:text-sm md:uppercase" href="#">Book Online</a>
       </nav>
+
+      <div class="md:hidden">
+        <button class="block">
+          <svg class="h-6 w-6 stroke-current stroke-2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M4 6h16M4 12h16M4 18h16" />
+          </svg>
+        </button>
+      </div>
     </div>
   </header>
 </template>
