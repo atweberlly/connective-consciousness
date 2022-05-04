7cf6864a9efaa2a18b84f5d39ebfda349cae1e4a
John Joaquin Valdez <javaldez1642qc@student.fatima.edu.ph>
2022-05-04 12:29:26 +0800

    Create Services section

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -62,6 +62,81 @@
       </div>
     </section>
 
+    <section class="bg-cyprus py-8 text-white sm:py-12 md:py-16 lg:py-20 xl:py-24">
+      <div class="container">
+        <div role="grid">
+          <div class="grid gap-8 lg:grid-cols-2 xl:items-center" role="row">
+            <div role="gridcell">
+              <div class="text-sm uppercase tracking-wider">Services &amp; Treatments</div>
+              <h2 class="mt-1 font-display text-2xl font-bold md:text-3xl lg:text-4xl xl:text-5xl">
+                We provide the best
+                <br class="hidden lg:block" />
+                prices for you.
+              </h2>
+              <div class="mt-8 space-y-4 divide-y divide-slate-700" role="list">
+                <div role="listitem">
+                  <div>
+                    <div class="text-slate-300 sm:flex sm:flex-row sm:items-center sm:justify-between">
+                      <h3 class="font-display text-xl font-bold">Myofascial Massage Treatments</h3>
+                      <div class="mt-1 font-semibold sm:mt-0">30 min.</div>
+                      <div class="font-display text-xl font-bold">$50.00</div>
+                    </div>
+                    <div class="mt-4">
+                      <p class="text-slate-400">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    </div>
+                  </div>
+                </div>
+
+                <div role="listitem">
+                  <div class="mt-4">
+                    <div class="text-slate-300 sm:flex sm:flex-row sm:items-center sm:justify-between">
+                      <h3 class="font-display text-xl font-bold">Myofascial Massage Treatments</h3>
+                      <div class="mt-1 font-semibold sm:mt-0">30 min.</div>
+                      <div class="font-display text-xl font-bold">$50.00</div>
+                    </div>
+                    <div class="mt-4">
+                      <p class="text-slate-400">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    </div>
+                  </div>
+                </div>
+
+                <div role="listitem">
+                  <div class="mt-4">
+                    <div class="text-slate-300 sm:flex sm:flex-row sm:items-center sm:justify-between">
+                      <h3 class="font-display text-xl font-bold">Myofascial Massage Treatments</h3>
+                      <div class="mt-1 font-semibold sm:mt-0">30 min.</div>
+                      <div class="font-display text-xl font-bold">$50.00</div>
+                    </div>
+                    <div class="mt-4">
+                      <p class="text-slate-400">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div role="gridcell">
+              <div role="grid">
+                <div class="grid grid-cols-3 gap-4 lg:grid-cols-1" role="row">
+                  <div role="gridcell">
+                    <img class="lg:mx-auto lg:w-40 xl:w-48" src="/images/hand-with-vines.png" alt="A hand with vines" />
+                  </div>
+
+                  <div role="gridcell">
+                    <img class="lg:mx-auto lg:w-40 xl:w-48" src="/images/cc-logomark.png" alt="Connective Consciousness" />
+                  </div>
+
+                  <div role="gridcell">
+                    <img class="lg:mx-auto lg:w-40 xl:w-48" src="/images/massage-demo.png" alt="Massage demo" />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
     <section class="bg-special-offer bg-cover bg-top bg-no-repeat py-8 text-white sm:py-16 md:py-20 xl:pt-28 xl:pb-36">
       <div class="container">
         <div class="text-sm uppercase tracking-wider">Special Offer</div>
